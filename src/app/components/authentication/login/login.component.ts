4e80277f4b2262eb88a45df65d58f8c0c9badc7e
sundicode <sundicharles2003@mail.com>
2024-02-23 14:25:47 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch ui-charles
    Changes to be committed:
    	modified:   src/app/components/authentication/auth-layout/auth-layout.component.html
    	modified:   src/app/components/authentication/auth-layout/auth-layout.component.scss
    	modified:   src/app/components/authentication/auth-layout/auth-layout.component.ts
    	modified:   src/app/components/authentication/login/login.component.html
    	modified:   src/app/components/authentication/login/login.component.scss
    	modified:   src/app/components/authentication/login/login.component.ts
    	modified:   src/app/components/authentication/register/register.component.html
    	new file:   src/app/shared/components/auth-card/auth-card.component.html
    	new file:   src/app/shared/components/auth-card/auth-card.component.scss
    	new file:   src/app/shared/components/auth-card/auth-card.component.spec.ts
    	new file:   src/app/shared/components/auth-card/auth-card.component.ts
    	new file:   src/assets/images/login/Forgot password-rafiki 1.png
    	new file:   src/assets/images/login/Logic-bro 1.png
    	new file:   src/assets/images/login/Sign up-bro 1.png
    	new file:   src/assets/images/logo/Titre du site.png
    	new file:   src/assets/images/logo/Waarfira-removebg-preview 3.png
    	modified:   src/assets/scss/utils/_variables.scss
    	modified:   src/styles.scss

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FeatherIconsComponent } from "../../../shared/components/feather-icons/feather-icons.component";
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { FeatherIconsComponent } from '../../../shared/components/feather-icons/feather-icons.component';
 import { RouterModule } from '@angular/router';
+import { AuthCardComponent } from '../../../shared/components/auth-card/auth-card.component';
+
+
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss',
-    imports: [FeatherIconsComponent,RouterModule]
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [FeatherIconsComponent, RouterModule, AuthCardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class LoginComponent implements OnInit {
-
   public show: boolean = false;
+  public loginIlustrations: string = ""
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   showPassword() {
     this.show = !this.show;
